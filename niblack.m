3f62705be3f4b8c64a0e6f5018043adbcd1764ec
JRIngram <jr-ingram@hotmail.co.uk>
2018-11-11 09:34:00 +0000

    Implementation of niblack

--- niblack.m
+++ niblack.m
@@ -1,7 +1,8 @@
-avg_filter = fspecial('average', 3) %Creates an average filter
-avg_hw1 = imfilter(HW1, avg_filter) %Averages the image
-std_hw1 = stdfilt(avg_hw1) %Calculates standard deviation
-k = 0.5;
+avg_filter = fspecial('average', 100); %Creates an average filter
+avg_hw1 = imfilter(HW1, avg_filter); %Averages the image
+std_hw1 = stdfilt(avg_hw1); %Calculates standard deviation
+k = 0.50;
 k_std_h1 = k .* std_hw1;
-niblack_threshold = double(avg_hw1) + k_std_h1
-%threshold_hw1 = threshold(hw1, 
+niblack_threshold = double(avg_hw1) + k_std_h1;
+threshold_hw1 = threshold(HW1, niblack_threshold);
+figure, axis off, image(threshold_hw1), colormap gray(256)
