9d9f177f989ead934da53e40a34630ab509e5727
JRIngram <jr-ingram@hotmail.co.uk>
2018-11-13 18:44:38 +0000

    Added PSNR calculation

---
+++ calculatePSNR.m
@@ -0,0 +1,9 @@
+function PSNR = calculatePSNR(img1,img2)
+%CALCULATEPSNR Summary of this function goes here
+%   Detailed explanation goes here
+pixel_diff = double(img1) - double(img2);
+MSE = pixel_diff.^2 ./ length(pixel_diff.^2);
+MSE = sum(MSE(:));
+PSNR = log10(256^2 ./ MSE)
+end
+
