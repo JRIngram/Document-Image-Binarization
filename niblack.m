6d40a2e7baf1107c32d1b660daf231c26bb17f0c
JRIngram <jr-ingram@hotmail.co.uk>
2018-11-13 18:29:35 +0000

    Changed niblack to a function

--- niblack.m
+++ niblack.m
@@ -1,8 +1,12 @@
-avg_filter = fspecial('average', 100); %Creates an average filter
-avg_hw1 = imfilter(HW1, avg_filter); %Averages the image
-std_hw1 = stdfilt(avg_hw1); %Calculates standard deviation
-k = 0.50;
-k_std_h1 = k .* std_hw1;
-niblack_threshold = double(avg_hw1) + k_std_h1;
-threshold_hw1 = threshold(HW1, niblack_threshold);
-figure, axis off, image(threshold_hw1), colormap gray(256)
+function niblackedImage = niblack(image, neighbourhoodSize,k)
+%NIBLACK Summary of this function goes here
+%   Detailed explanation goes here
+    avg_filter = fspecial('average', neighbourhoodSize); %Creates an average filter
+    avgImage = imfilter(image, avg_filter); %Averages the image
+    stdImage = stdfilt(avgImage); %Calculates standard deviation
+    kStdImage = k .* stdImage;
+    niblackThreshold = double(avgImage) + kStdImage;
+    niblackedImage = threshold(image, niblackThreshold);
+
+end
+
