78aa87ac6f10f7c7028638f868ccd95d4d956c6f
Jamie Ingram <jr-ingram@hotmail.co.uk>
2018-11-21 22:05:56 +0000

    generate_results.m display now shows niblacked images

--- generate_results.m
+++ generate_results.m
@@ -1,20 +1,45 @@
 %calculate_all_neighbour_PSNRs;
 load_and_grey_test_images;
 
+%Binarizes images using Otsu's method
 threshold = graythresh(HW1);
 HW1Otsu = convert_to_binary(HW1, threshold);
-
 threshold = graythresh(HW4);
 HW4Otsu = convert_to_binary(HW4, threshold);
-
 threshold = graythresh(PR4);
 PR4Otsu = convert_to_binary(PR4, threshold);
-
 threshold = graythresh(PR7);
 PR7Otsu = convert_to_binary(PR7, threshold);
+
+k = -0.2 %K constant value
+%Binarize images using Niblack's method, with lowest PNSR neighbourhood.
+HW1NibMin = niblack(HW1, 3, k);
+HW4NibMin = niblack(HW4, 3, k);
+PR4NibMin = niblack(PR4, 5, k);
+PR7NibMin = niblack(PR7, 5, k);
+
+%Binarize images using Niblack's method, with greatest PNSR neighbourhood.
+HW1NibMax = niblack(HW1, 537, k);
+HW4NibMax = niblack(HW4, 665, k);
+PR4NibMax = niblack(PR4, 875, k);
+PR7NibMax = niblack(PR7, 573, k);
 
 %Displays images that have been binarized using Otsu's method.
 figure, image(HW1Otsu), axis off, colormap gray(2)
 figure, image(HW4Otsu), axis off, colormap gray(2)
 figure, image(PR4Otsu), axis off, colormap gray(2)
 figure, image(PR7Otsu), axis off, colormap gray(2)
+
+%Displayed Images that have been binarized using Niblack's method (with a
+%small neighbourhood size).
+figure, image(HW1NibMin), axis off, colormap gray(2)
+figure, image(HW4NibMin), axis off, colormap gray(2)
+figure, image(PR4NibMin), axis off, colormap gray(2)
+figure, image(PR7NibMin), axis off, colormap gray(2)
+
+%Displayed Images that have been binarized using Niblack's method (with a
+%small neighbourhood size).
+figure, image(HW1NibMax), axis off, colormap gray(2)
+figure, image(HW4NibMax), axis off, colormap gray(2)
+figure, image(PR4NibMax), axis off, colormap gray(2)
+figure, image(PR7NibMax), axis off, colormap gray(2)
