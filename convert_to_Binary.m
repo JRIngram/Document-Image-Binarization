0f260007afe45adf4274348a69719444ff067229
Jamie Ingram <jr-ingram@hotmail.co.uk>
2018-11-23 10:04:59 +0000

    Improving docs

--- convert_to_Binary.m
+++ convert_to_Binary.m
@@ -1,6 +1,6 @@
 function binaryImage = convert_to_binary(uint8Image)
 %CONVERT_TO_BINARY Converts a uint8 image to a binary image
-binaryImage = imbinarize(uint8Image);
-binaryImage = uint8(binaryImage);
+    binaryImage = imbinarize(uint8Image);
+    binaryImage = uint8(binaryImage);
 end
 
